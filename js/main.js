68962184465ba99e0ba865d23416c543ba0d36a2
Ishan Madhusanka <ahtimadhusanka@gmail.com>
2017-07-28 13:50:17 +0530

    adding my resume + vue.js

--- js/main.js
+++ js/main.js
@@ -1,4 +1,8 @@
+
 document.addEventListener('DOMContentLoaded', function () {
+
+    var _lock = 4;
+
     axios.get('/header.html')
         .then(function (res) {
             document.getElementById('header').innerHTML = res.data;
@@ -10,6 +14,12 @@ document.addEventListener('DOMContentLoaded', function () {
             var profile = document.getElementById('profile');
 
             function checkScroll(scroll_pos) {
+                if(header.clientHeight == 0) {
+                    header = document.getElementById('header');
+                    parallax = document.getElementById('parallax');
+                    profile = document.getElementById('profile');
+                }
+
                 if (scroll_pos < header.clientHeight) {
                     parallax.style.transform = 'translateY(' + scroll_pos*0.5 + 'px)';
                     profile.style.transform = 'translateY(' + scroll_pos*0.4 + 'px)';
@@ -27,15 +37,61 @@ document.addEventListener('DOMContentLoaded', function () {
                 ticking = true;
             });
 
+            _lock--;
+            if(_lock == 0){
+                initialize_app();
+            }
+
         });
     axios.get('/content.html')
         .then(function (res) {
             document.getElementById('content').innerHTML = res.data;
+
+            _lock--;
+            if(_lock == 0){
+                initialize_app();
+            }
         });
     axios.get('/footer.html')
         .then(function (res) {
             document.getElementById('footer').innerHTML = res.data;
+
+            _lock--;
+            if(_lock == 0){
+                initialize_app();
+            }
+        });
+    axios.get('/resume.html')
+        .then(function (res) {
+            document.getElementById('resume').innerHTML = res.data;
+
+            _lock--;
+            if(_lock == 0){
+                initialize_app();
+            }
         });
 
+    function initialize_app(){
+        console.log('[imadx.github.io] initializing app...')
+        var app = new Vue({
+            el: '#app',
+            data: {
+                visible_resume: true,
+            },
+            methods: {
+                showResume: function(){
+                    console.log('[imadx.github.io] switching to resume...')
+                    this.visible_resume = true;
+                },
+                hideResume: function(){
+                    console.log('[imadx.github.io] switching to homepage...')
+                    this.visible_resume = false;
+                }
+            },
+            watch: {
+            }
+        })
+            
+    }
 
 });
